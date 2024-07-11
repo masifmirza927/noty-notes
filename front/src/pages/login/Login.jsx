fb61502a7de914788402fb7c13048751dfd9929a
Asif <masifmirza927@gmail.com>
2024-07-11 22:59:22 +0500

    login form

--- front/src/pages/login/Login.jsx
+++ front/src/pages/login/Login.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+
   return (
     <div id='login' className='login-page'>
-        <div className='login-form'>
-            <h1>login form</h1>
-        </div>
+      <div className='login-form p-10'>
+        <h1 className='text-center text-3xl font-bold my-3 mb-10'>login form</h1>
+        <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+          <div className="mb-10">
+            <input type='text' {...register('username', { required: { value: true, message: "username is required" } })} className='w-full p-5 rounded-md bg-transparent border text-white' placeholder='Email address' />
+            {errors.username && <p className="text-red-600 my-3 bg-gray-50 p-2">
+              <span>{errors.username.message}</span>
+            </p>
+            }
+          </div>
+
+          <input type='password' {...register('password')} placeholder='Password' className='w-full p-5 mb-3 rounded-md bg-transparent border text-white' />
+          <button className='w-full p-5 mb-3 rounded-md bg-gray-900 text-white border-0 text-xl mt-3 hover:bg-gray-800'>Login to noty</button>
+        </form>
+      </div>
     </div>
   )
 }
